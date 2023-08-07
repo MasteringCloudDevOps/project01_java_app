84cb6e6ec86ca2b390e8d069d38dd8901df8086a
dhirendra <dhirendra@gmail.com>
2023-08-07 16:09:40 +0000

    modified jenkinsfile and shared library

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,7 @@
+def call(map stageParams) {
+    checkout scmGit(
+            branches: [[name: stageParams.branch]],
+            extensions: [], 
+            userRemoteConfigs: [[url: stageParams.url]]
+            )
+}
